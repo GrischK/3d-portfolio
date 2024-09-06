a21f16a45a23eb9d731b8124cabf1c1a1244f373
GrischK <grischka.gorski@laposte.net>
2024-09-06 23:52:31 +0200

    ✨ Add email service

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {Home, About, Projects, Contact} from "./pages"
-import NavBar from "./components/NavBar.jsx";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Home, About, Projects, Contact } from './pages';
+import NavBar from './components/NavBar.jsx';
 
 const App = () => {
-    return (
-        <main className="text-3xl font-bold text-blue-500">
-            <BrowserRouter>
-                <NavBar/>
-                <Routes>
-                    <Route path="/" element={<Home/>}/>
-                    <Route path="/about" element={<About/>}/>
-                    <Route path="/projects" element={<Projects/>}/>
-                    <Route path="/projects" element={<Contact/>}/>
-                </Routes>
-            </BrowserRouter>
-        </main>
-    )
-}
+  return (
+    <main className="text-3xl font-bold text-blue-500">
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="/about"
+            element={<About />}
+          />
+          <Route
+            path="/projects"
+            element={<Projects />}
+          />
+          <Route
+            path="/contact"
+            element={<Contact />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </main>
+  );
+};
 
 export default App;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,108 @@
+import { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+
 const Contact = () => {
-    return (
-        <div className="text-3xl text-red-600">Contact</div>
-    )
-}
+  const formRef = useRef(null);
+  const [form, setForm] = useState({ name: '', email: '', message: '' });
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  const handleFocus = (e) => {};
+  const handleBlur = (e) => {};
+
+  console.log(import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY);
+  console.log(import.meta.env.VITE_APP_EMAILJS_SERVICE);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    console.log(import.meta.env.APP_EMAILJS_PUBLIC_KEY);
+    console.log(import.meta.env.APP_EMAILJS_SERVICE);
+
+    emailjs
+      .send(
+        import.meta.env.VITE_APP_EMAILJS_SERVICE,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE,
+        {
+          from_name: form.name,
+          to_name: 'Grisch',
+          from_email: form.email,
+          to_email: 'contact@dev.com',
+          message: form.message
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(() => {
+        setIsLoading(false);
+        setForm({ name: '', email: '', message: '' });
+      });
+  };
+
+  return (
+    <section className="relative flex lg:flex-row flex-col max-container">
+      <div className="flex-1 min-w-[50%] flex flex-col">
+        <h1 className="head-text">Get in touch</h1>
+        <form
+          className="w-full flex flex-col gap-7 mt-14"
+          onSubmit={handleSubmit}
+        >
+          <label className="text-black-500 font-semibold text-lg">
+            Name
+            <input
+              type="text"
+              name="name"
+              className="input"
+              required
+              placeholder="John"
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className="text-black-500 font-semibold text-lg">
+            Email
+            <input
+              type="text"
+              name="email"
+              className="input"
+              required
+              placeholder="john@gmail.com"
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className="text-black-500 font-semibold text-lg">
+            Message
+            <textarea
+              name="message"
+              rows="4"
+              className="textarea"
+              required
+              placeholder="Let me know how I can help you!"
+              value={form.message}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <button
+            type="submit"
+            className="btn"
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            disabled={isLoading}
+          >
+            {isLoading ? 'Sending' : 'Send Message'}
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
 
 export default Contact;
