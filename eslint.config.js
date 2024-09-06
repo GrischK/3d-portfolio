1c6431074610fe211f97eca243d7371d0c370308
GrischK <grischka.gorski@laposte.net>
2024-09-06 11:33:08 +0200

    ⬆️ Add prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,15 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import prettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 export default [
-  { ignores: ['dist'] },
+  {
+    ignores: ['dist', 'src']
+  },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
@@ -14,25 +18,25 @@ export default [
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+        sourceType: 'module'
+      }
     },
     settings: { react: { version: '18.3' } },
     plugins: {
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier: prettierPlugin
     },
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
+      ...prettier.rules,
+      'prettier/prettier': 'error',
       'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-]
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
+    }
+  }
+];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,48 +1,52 @@
-import {Canvas} from "@react-three/fiber";
-import {Suspense} from "react";
-import Loader from "../components/Loader.jsx";
-import Island from "../models/Island";
+import { Canvas } from '@react-three/fiber';
+import { Suspense } from 'react';
+import Loader from '../components/Loader.jsx';
+import Island from '../models/Island';
 
 // <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
 //   Home Pop-Up
 // </div>
 
 const Home = () => {
-  const adjustIslandForScreenSize = ()=>{
-    let screenScale=null
-    let screenPosition = [0,-6.5,-43]
-    let rotation = [0.1,4.7,0]
-    if(window.innerWidth < 768){
-      screenScale=[0.9,0.9,0.9]
-    }else {
-      screenScale=[1,1,1]
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
     }
-    return [screenScale, screenPosition,rotation]
-  }
+    return [screenScale, screenPosition, rotation];
+  };
 
-  const [islandScale, islandPosition,islandRotation] = adjustIslandForScreenSize();
+  const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
 
   return (
     <section className="w-full h-screen relative">
       <p>Home</p>
-      <Canvas className="w-full h-screen bg-transparent"
-              camera={{near: 0.1, far: 1000}}
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
       >
-        <Suspense fallback={<Loader/>}>
-          <directionalLight/>
-          <ambientLight/>
-          <pointLight/>
-          <spotLight/>
-          <hemisphereLight/>
+        <Suspense fallback={<Loader />}>
+          <directionalLight
+            position={[1, 1, 1]}
+            intensity={2}
+          />
+          <ambientLight intensity={0.5} />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
           <Island
-          position={islandPosition}
-          scale={islandScale}
-          rotation={islandRotation}
+            position={islandPosition}
+            scale={islandScale}
+            rotation={islandRotation}
           />
         </Suspense>
       </Canvas>
     </section>
-  )
-}
+  );
+};
 
 export default Home;
