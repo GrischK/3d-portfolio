4be7a7c740b5f15ab37bb717e673a9caf7963c87
GrischK <grischka.gorski@laposte.net>
2024-09-09 17:36:40 +0200

    ⚡️ Adjust island rotation

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -59,14 +59,14 @@ export function Island({ isRotating, setIsRotating, setCurrentStage, ...props })
       if (!isRotating) {
         setIsRotating(true);
       }
-      islandRef.current.rotation.y += 0.005 * Math.PI;
-      rotationSpeed.current = 0.007;
+      islandRef.current.rotation.y += 0.01 * Math.PI;
+      rotationSpeed.current = 0.0125;
     } else if (e.key === 'ArrowRight') {
       if (!isRotating) {
         setIsRotating(true);
       }
-      islandRef.current.rotation.y -= 0.005 * Math.PI;
-      rotationSpeed.current = -0.007;
+      islandRef.current.rotation.y -= 0.01 * Math.PI;
+      rotationSpeed.current = -0.0125;
     }
   };
 
