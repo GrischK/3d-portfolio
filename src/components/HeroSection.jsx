c5aa235405d28515a87f854d95fa552138a57ea6
GrischK <grischka.gorski@laposte.net>
2024-09-20 11:59:05 +0200

    🚧 Camera zoom transition

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,39 +1,67 @@
-import { Environment, OrbitControls, Text } from '@react-three/drei';
+import { CameraControls, Environment, Text } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import { Winter } from '../models/Winter.jsx';
 import { degToRad } from 'maath/misc';
+import { Suspense, useEffect, useRef, useState } from 'react';
+import Loader from './Loader.jsx';
 
 const HeroSection = () => {
+  const controls = useRef();
+
+  const intro = async () => {
+    controls.current.dolly(-22);
+    controls.current.smoothTime = 1.6;
+    controls.current.dolly(22, true);
+  };
+
+  useEffect(() => {
+    // Utiliser un setTimeout pour vérifier la disponibilité du ref après un petit délai
+    setTimeout(() => {
+      if (controls.current) {
+        console.log('Controls:', controls.current);
+        // Appeler la fonction intro ici si nécessaire
+        intro();
+      } else {
+        console.log('controls.current est toujours undefined');
+      }
+    }, 500); // 100ms pour laisser le temps à CameraControls de se monter
+  }, []); // Pas besoin d'ajouter une dépendance sur controls.current ici
+
   return (
     <Canvas
       shadows
-      camera={{ position: [0, 0, 8], fov: 42 }}
+      camera={{
+        position: [0, 0, 8],
+        fov: 42
+      }}
       style={{ height: '100vh', width: '100vw' }}
     >
-      <color
-        attach="background"
-        args={['#171720']}
-      />
-      <OrbitControls />
-      <Text
-        font="fonts/Poppins-Black.ttf"
-        fontSize={0.8}
-        position={[-1.3, -0.5, 1]}
-        lineHeight={0.8}
-        textAlign="center"
-        color="white"
-        rotation-y={degToRad(30)}
-        // anchorY={'bottom'}
-      >
-        GRISCHKA{'\n'}GORSKI
-      </Text>
-      <group
-        rotation-y={degToRad(160)}
-        position-x={3}
-      >
-        <Winter scale={0.7} />
-      </group>
-      <Environment preset="sunset" />
+      <Suspense fallback={<Loader />}>
+        <color
+          attach="background"
+          args={['#171720']}
+        />
+        <CameraControls ref={controls} />
+        <Text
+          font="fonts/Poppins-Black.ttf"
+          fontSize={0.8}
+          position={[-1.3, -0.5, 1]}
+          lineHeight={0.8}
+          textAlign="center"
+          color="white"
+          rotation-y={degToRad(30)}
+          // anchorY={'bottom'}
+        >
+          GRISCHKA{'\n'}GORSKI
+        </Text>
+        <group
+          rotation-y={degToRad(160)}
+          position-x={3}
+        >
+          <Winter scale={0.7} />
+        </group>
+        <Environment preset="sunset" />
+      </Suspense>
     </Canvas>
   );
 };
