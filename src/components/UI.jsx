23113ac862d07cac24dac13ae5d73ca5a3bd9a65
GrischK <grischka.gorski@laposte.net>
2024-11-04 13:50:50 +0100

    💫 Config tailwind animations

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -84,29 +84,29 @@ export const UI = () => {
       >
         <div className="relative -rotate-2">
           <div className="bg-white/0 animate-horizontal-scroll flex items-center gap-8 w-max px-8">
-            <h1 className="shrink-0 text-white text-10xl font-black ">Dev Grisch</h1>
-            <h2 className="shrink-0 text-white text-8xl italic font-light">React Three Fiber</h2>
+            <h1 className="shrink-0 text-white text-10xl font-black ">React Three Fiber</h1>
+            <h2 className="shrink-0 text-white text-8xl italic font-light">Dev Grisch</h2>
             <h2 className="shrink-0 text-white text-12xl font-bold">Three.js</h2>
             <h2 className="shrink-0 text-transparent text-12xl font-bold italic outline-text">
               Portfolio
             </h2>
             <h2 className="shrink-0 text-white text-9xl font-medium">Passionate</h2>
             <h2 className="shrink-0 text-white text-9xl font-extralight italic">Dedicated</h2>
-            <h2 className="shrink-0 text-white text-13xl font-bold">Involved</h2>
+            <h2 className="shrink-0 text-white text-9xl font-medium">Involved</h2>
             <h2 className="shrink-0 text-transparent text-13xl font-bold outline-text italic">
               Creative
             </h2>
           </div>
           <div className="absolute top-0 left-0 bg-white/0 animate-horizontal-scroll-2 flex items-center gap-8 px-8 w-max">
-            <h1 className="shrink-0 text-white text-10xl font-black ">Dev Grisch</h1>
-            <h2 className="shrink-0 text-white text-8xl italic font-light">React Three Fiber</h2>
+            <h1 className="shrink-0 text-white text-10xl font-black ">React Three Fiber</h1>
+            <h2 className="shrink-0 text-white text-8xl italic font-light">Dev Grisch</h2>
             <h2 className="shrink-0 text-white text-12xl font-bold">Three.js</h2>
             <h2 className="shrink-0 text-transparent text-12xl font-bold italic outline-text">
               Portfolio
             </h2>
-            <h2 className="shrink-0 text-white text-9xl font-medium">Passionate</h2>
-            <h2 className="shrink-0 text-white text-9xl font-extralight italic">Dedicated</h2>
-            <h2 className="shrink-0 text-white text-13xl font-bold">Involved</h2>
+            <h2 className="shrink-0 text-white text-9xl font-extralight italic">Passionate</h2>
+            <h2 className="shrink-0 text-white text-9xl font-medium">Dedicated</h2>
+            <h2 className="shrink-0 text-white text-9xl font-extralight italic">Involved</h2>
             <h2 className="shrink-0 text-transparent text-13xl font-bold outline-text italic">
               Creative
             </h2>
--- tailwind.config.js
+++ tailwind.config.js
@@ -21,7 +21,21 @@ export default {
       },
       boxShadow: {
         card: '0px 1px 2px 0px rgba(0, 0, 0, 0.05)'
-      }
+      },
+      animation: {
+        "horizontal-scroll": "horizontal-scroll linear 16s infinite ",
+        "horizontal-scroll-2": "horizontal-scroll-2 linear 16s infinite ",
+      },
+      keyframes: {
+        "horizontal-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
+        "horizontal-scroll-2": {
+          from: { transform: "translateX(100%)" },
+          to: { transform: "translateX(0)" },
+        },
+      },
     }
   },
   plugins: []
