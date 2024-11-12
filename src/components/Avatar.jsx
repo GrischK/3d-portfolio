d9e1dd8cd0b53b4bf4c0965a0ece1e5f33a21e97
GrischK <grischka.gorski@laposte.net>
2024-11-12 17:23:08 +0100

    🚧 Create scene

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,16 +1,24 @@
-import { MeshReflectorMaterial, OrbitControls, PresentationControls, Stage } from '@react-three/drei';
+import { MeshReflectorMaterial, PresentationControls, Stage } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
+import { Car } from '../models/Car.jsx';
+import { Suspense } from 'react';
 
 const Avatar = () => {
   return (
-    <Canvas style={{ height: '100vh', width: '100vw', backgroundColor: 'black' }}>
-      <color attach="background" args={["#213547"]} />
-      <fog attach="fog" args={["#213547", 10, 20]} />
+    <Canvas style={{ height: '100vh', width: '100vw', backgroundColor: 'black' }} camera={{ position: [-0.5, 1, 8]}}>
+      <color
+        attach="background"
+        args={['#213547']}
+      />
+      <fog
+        attach="fog"
+        args={['#213547', 10, 30]}
+      />
       <>
         <PresentationControls
           speed={1.5}
           global
-          polar={[-0.1, Math.PI / 4]}
+          polar={[-0.5, Math.PI / 4]}
           rotation={[Math.PI / 8, Math.PI / 4, 0]}
         >
           <Stage
@@ -19,20 +27,19 @@ const Avatar = () => {
             castShadow={false}
             adjustCamera={false}
           >
-            <mesh>
-              <boxGeometry />
-              <meshNormalMaterial />
-            </mesh>
+            <Suspense fallback={null}>
+              <Car />
+            </Suspense>
           </Stage>
           <mesh
             rotation={[-Math.PI / 2, 0, 0]}
-            position-y={-2}
+            position-y={-1.3}
           >
             <planeGeometry args={[170, 170]} />
             <MeshReflectorMaterial
               blur={[300, 100]}
               resolution={2048}
-              mixBlur={1}
+              mixBlur={0.5}
               mixStrength={40}
               roughness={1}
               depthScale={1.2}
