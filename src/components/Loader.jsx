8af7dbe6ce01c489fe622c0732afeabfc7aa7180
GrischK <grischka.gorski@laposte.net>
2025-05-14 17:17:51 +0200

    ⏪️ Revert changes

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,4 @@
 import { useProgress } from '@react-three/drei';
-import dev from '../assets/icons/dev.png';
 
 const Loader = ({ started, onStarted, isPlaying }) => {
   const handleClick = () => {
@@ -11,18 +10,10 @@ const Loader = ({ started, onStarted, isPlaying }) => {
   return (
     <div className={`loadingScreen ${started ? 'loadingScreen--started' : ''}`}>
       <div className="loadingScreen__board flex flex-col items-center sm:items-stretch">
-        <div className="flex items-center justify-between w-full">
-          <h1 className="sm:text-8xl text-4xl text-[#1d3b16] font-bold sm:text-left text-center">
-            Welcome in my
-            <br /> little universe!
-          </h1>
-          <img
-            src={dev}
-            alt={'dev'}
-            className={'h-36 w-36'}
-          />
-        </div>
-
+        <h1 className="sm:text-8xl text-4xl text-[#1d3b16] font-bold sm:text-left text-center">
+          Welcome in my
+          <br /> little universe!
+        </h1>
         <p className="sm:text-4xl text-lg pt-4 px-8 text-black mx-5 font-semibold sm:text-right text-center">
           I'm Grischka, a Full-stack developer passionate about building efficient and intuitive
           applications.
